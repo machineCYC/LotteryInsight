4a628ffffb7fd88f889d396aaa524e16aae53b42
YenChen <yenchen0416@gmail.com>
2021-06-21 19:47:51 +0800

    add: db information in github secrets

--- genenv.py
+++ genenv.py
@@ -1,25 +1,17 @@
 import os
-from configparser import ConfigParser
 
 
-# def get_config_from_environment(env_content):
-#     # get config from env
-#     env_content += "MYSQL_HOST={}\n".format(os.environ.get("MYSQL_HOST", ""))
-#     env_content += "MYSQL_USER={}\n".format(os.environ.get("MYSQL_USER", ""))
-#     env_content += "MYSQL_PASSWORD={}\n".format(os.environ.get("MYSQL_PASSWORD", ""))
-#     return env_content
+def get_config_from_environment(env_content):
+    # get config from env
+    env_content += "MYSQL_HOST={}\n".format(os.environ.get("MYSQL_HOST", ""))
+    env_content += "MYSQL_USER={}\n".format(os.environ.get("MYSQL_USER", ""))
+    env_content += "MYSQL_PASSWORD={}\n".format(os.environ.get("MYSQL_PASSWORD", ""))
+    env_content += "MYSQL_PORT={}\n".format(os.environ.get("MYSQL_PORT", ""))
+    return env_content
 
-local_config = ConfigParser()
-local_config.read("config.ini")
-
-status = os.environ.get("VERSION", "")
-section = local_config[status] if status else local_config["DEFAULT"]
 
 env_content = ""
-for sec in section:
-    env_content += "{}={}\n".format(sec.upper(), section[sec])
-
-# env_content = get_config_from_environment(env_content)
+env_content = get_config_from_environment(env_content)
 
 with open(".env", "w", encoding="utf8") as env:
     env.write(env_content)
